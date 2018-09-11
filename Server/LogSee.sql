005decba2e0660eecbcbbe4b9f1c4a37ee46a87e
ainsey11 <ainsey11@gmail.com>
2018-09-11 15:54:57 -0700

    Added more tables in, fairly self explanatory

--- Server/LogSee.sql
+++ Server/LogSee.sql
@@ -1,8 +1,36 @@
-create table `Client_Auth` (
-`ID` int(10) NOT NULL AUTO_INCRAMENT,
+create table `client_auth` (
+`id` int(10) NOT NULL AUTO_INCRAMENT,
 `clientid` varchar(20) NOT NULL,
 `username` varchar(20) NOT NULL,
 `password` varchar(50) NOT NULL,
-primary key (`ID`)
-)
+primary key (`id`)
+);
+
+create table `admin_auth` (
+`id` int(10) NOT NULL AUTO_INCRAMENT,
+`username` varchar(20) NOT NULL,
+`password` varchar(50) NOT NULL,
+`email` varchar(65) NULL,
+`verified` tinyint(1) NOT NULL DEFAULT '0',
+`role` varchar(20) NOT NULL,
+`mod_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+`last_login` datetime NOT NULL,
+primary key (`id`)
+);
+
+create table `auth_audit_log` (
+`id` int(1000) NOT NULL AUTO_INCRAMENT,
+`time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+`username` varchar(20) NOT NULL,
+`user_ip` varchar(20) NOT NULL,
+`status` tinyint(1) NOT NULL
+primary key (`id`)
+);
+
+create table `logsee_serverstatus` (
+`component` varchar(20) NOT NULL,
+`status` tinyint(1) NOT NULL
+primary key (`component`)
+);
+
 
