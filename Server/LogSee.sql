95aca6c62a0bc5ff449ad97aae2152b2ed1a1b0d
ainsey11 <ainsey11@gmail.com>
2018-09-11 15:23:18 -0700

    Making a start on the mysql schema, this is the credential table for the client streamer api authentication

---
+++ Server/LogSee.sql
@@ -0,0 +1,8 @@
+create table `Client_Auth` (
+`ID` int(10) NOT NULL AUTO_INCRAMENT,
+`clientid` varchar(20) NOT NULL,
+`username` varchar(20) NOT NULL,
+`password` varchar(50) NOT NULL,
+primary key (`ID`)
+)
+
