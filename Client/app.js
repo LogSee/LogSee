748035759f92d9ad2747687f68c85a42ad56de3c
P0pzi <tburtun@gmail.com>
2018-09-11 22:38:37 +0100

    Unsure about PopulateMeta()

--- Client/app.js
+++ Client/app.js
@@ -4,36 +4,46 @@ var path = require('path') // For managing paths, ofcourse.
 // Parse config file
 var config = JSON.parse(fs.readFileSync('./Client/config.json', 'utf8'));
 
-function Init() {
-    
+function Init() { // Same code to below but slightly different, I feel like it could be optimized somehow...
     for (var f = 0; f < config['PathsToScan'].length; f++) {
-
-        // Does this dir/file actually exist on our system?
-        if (fs.existsSync(config.PathsToScan[f].Location)) {
+        if (fs.existsSync(config.PathsToScan[f].Location)) { // Does this dir/file actually exist on our system?
             var tf = config.PathsToScan[f] // This File/dir (tf)
-
-            // Is it a file or a directory?
-            if (fs.lstatSync(config.PathsToScan[f].Location).isFile()) {
+            if (fs.lstatSync(config.PathsToScan[f].Location).isFile()) { // Is it a file or a directory?
                 // It's a file.
-                
                 //
-                // Todo: iterate over the PathsToScan and see if the database has data on them. If so: count how many lines the database has stored and continue from there.
+                // Todo: iterate over the PathsToScan and see if the database has data on them. If so: count how many lines the database has stored and continue from there. We need to make the API first.
                 //
-
             } else if (fs.lstatSync(config.PathsToScan[f].Location).isDirectory()) {
                 // It's a directory.
-
-                // Do some stuff (PG)
-                //console.log('Directory Statement Executed');
+                //
+                // Todo: iterate over the PathsToScan and see if the database has data on them. If so: count how many lines the database has stored and continue from there. We need to make the API first.
+                //
             } else {
                 console.warn(`[Warning] I'm unable to detect what "${config.PathsToScan[f].Location}" is. It may be a socket or symlink.`);
             };
-
         } else {
             console.warn(`[Warning] The file "${config.PathsToScan[f].Location}" does not exist!`);
         };
     };
 }
+
+// Populates config data with name and size.
+function PopulateMeta(obj) {
+    if (!obj.Location) {
+        console.warn(`[Warning] Location missing for ${obj}`);
+        return false;
+    };
+    // Have we seperated its file name for ease?    Note: May want to put in an init() func
+    if (!obj.filename) {
+        obj.filename = path.basename(obj.Location);
+    };
+    // Have we made note of it's size in memory?    Note: May want to put in an init() func
+    if (!obj.size) {
+        obj.size = fs.statSync(obj.Location).size;
+        console.log(`New size added for file "${obj.filename}" (${obj.size})`)
+    };
+    return true;
+};
 
 function ScanFiles() {
     setInterval(function() {
@@ -48,16 +58,7 @@ function ScanFiles() {
                 if (fs.lstatSync(config.PathsToScan[f].Location).isFile()) {
                     // It's a file.
                     
-                    // Have we seperated its file name for ease?    Note: May want to put in an init() func
-                    if (!tf.filename) {
-                        tf.filename = path.basename(tf.Location);
-                    };
-
-                    // Have we made note of it's size in memory?    Note: May want to put in an init() func
-                    if (!tf.size) {
-                        tf.size = fs.statSync(tf.Location).size;
-                        console.log(`New size added for file "${tf.filename}" (${tf.size})`)
-                    };
+                    PopulateMeta(tf);
 
                     // If the byte size != the previously logged byte size for that item, read it.
                     if (fs.statSync(tf.Location).size != tf.size) {
@@ -65,10 +66,13 @@ function ScanFiles() {
                         tf.size = fs.statSync(tf.Location).size; // Update its file size
                     };
                 } else if (fs.lstatSync(config.PathsToScan[f].Location).isDirectory()) {
-                    // It's a directory.
+                    // It's a directory, loop through all the .log (but not .log.*) files
+                    fs.readdirSync(tf.Location).forEach(function(filename) {
+                        if (path.extname(filename) == ".log") {
+                            console.log(filename);
+                        };
+                    });
 
-                    // Do some stuff (PG)
-                    //console.log('Directory Statement Executed');
                 } else {
                     console.warn(`[Warning] I'm unable to detect what "${config.PathsToScan[f].Location}" is. It may be a socket or symlink.`);
                 };
