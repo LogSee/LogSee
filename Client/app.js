5bf4e0e9260c17fd609c6356e2b4168fe503289b
P0pzi <tburtun@gmail.com>
2018-09-11 21:30:05 +0100

    Interval scanning
    Saftey check for it running too fast and hogging system resources

--- Client/app.js
+++ Client/app.js
@@ -5,35 +5,48 @@ var path = require('path') // For managing paths, ofcourse.
 var config = JSON.parse(fs.readFileSync('./Client/config.json', 'utf8'));
 
 
+function ScanFiles() {
 
-// Iterate over each file
-for (var f = 0; f < config['PathsToScan'].length; f++) {
+    setInterval(function() {
+        // Iterate over each file
+        for (var f = 0; f < config['PathsToScan'].length; f++) {
 
-    // Does this dir/file actually exist?
-    if (fs.existsSync(config.PathsToScan[f].Location)) {
-        var tf = config.PathsToScan[f] // This File (tf)
+            // Does this dir/file actually exist on our system?
+            if (fs.existsSync(config.PathsToScan[f].Location)) {
+                var tf = config.PathsToScan[f] // This File/dir (tf)
 
-        // Have we made note of it's size in memory?    Note: May want to put in an init() func
-        if (!tf.size) {
-            tf.size = fs.statSync(tf.Location).size;
-            console.log('New size added for file', tf.Location)
+                // Is it a file or a directory?
+                if (fs.lstatSync(config.PathsToScan[f].Location).isFile()) {
+                    // It's a file.
+                    
+                    // Have we made note of it's size in memory?    Note: May want to put in an init() func
+                    if (!tf.size) {
+                        tf.size = fs.statSync(tf.Location).size;
+                        console.log('New size added for file', tf.Location)
+                    };
+
+                    // Have we seperated its file name for ease?    Note: May want to put in an init() func
+                    if (!tf.filename) {
+                        tf.filename = path.basename(tf.Location);
+                    };
+
+                    // If the byte size != the previously logged byte size for that item, read it.
+                    console.log(fs.statSync(tf.Location).size);
+
+                } else if (fs.lstatSync(config.PathsToScan[f].Location).isDirectory()) {
+                    // It's a directory.
+
+                    // Do some stuff (PG)
+
+                } else {
+                    console.warn('[Warning] I\'m unable to detect what "', config.PathsToScan[f].Location, '" is. It may be a socket or symlink.')
+                };
+
+            } else {
+                console.warn('[Warning] The file', config.PathsToScan[f].Location, 'does not exist!')
+            };
         };
+    }, config.Client.ScanFrequency)
+};
 
-        // Have we seperated its file name?             Note: May want to put in an init() func
-        if (!tf.filename) {
-            tf.filename = path.basename(tf.Location);
-        }
-
-        // Is this a directory (Are we scanning every .log file?)
-        if (tf.ScanDirectory) {
-            // Do some shit.
-        };
-
-        // If the byte size != the previously logged byte size for that item, read it.
-        console.log(fs.statSync(tf.Location).size);
-
-
-    } else {
-        console.warn('[Warning] The file', config.PathsToScan[f].Location, 'does not exist!')
-    };
-}
+ScanFiles();
