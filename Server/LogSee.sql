2bb3c210b6b5ae586b79b4a78c7e2ef4b06d41c3
ainsey11 <ainsey11@gmail.com>
2018-09-12 11:01:02 -0700

    changing to CamelCase

--- Server/LogSee.sql
+++ Server/LogSee.sql
@@ -1,36 +1,36 @@
 create table `client_auth` (
-`id` int(10) NOT NULL AUTO_INCRAMENT,
-`clientid` varchar(20) NOT NULL,
-`username` varchar(20) NOT NULL,
-`password` varchar(50) NOT NULL,
-primary key (`id`)
+`Id` int(10) NOT NULL AUTO_INCRAMENT,
+`ClientId` varchar(20) NOT NULL,
+`Username` varchar(20) NOT NULL,
+`Password` varchar(50) NOT NULL,
+primary key (`Id`)
 );
 
 create table `admin_auth` (
-`id` int(10) NOT NULL AUTO_INCRAMENT,
-`username` varchar(20) NOT NULL,
-`password` varchar(50) NOT NULL,
-`email` varchar(65) NULL,
-`verified` tinyint(1) NOT NULL DEFAULT '0',
-`role` varchar(20) NOT NULL,
-`mod_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+`Id` int(10) NOT NULL AUTO_INCRAMENT,
+`Username` varchar(20) NOT NULL,
+`Password` varchar(50) NOT NULL,
+`Email` varchar(65) NULL,
+`Verified` tinyint(1) NOT NULL DEFAULT '0',
+`Role` varchar(20) NOT NULL,
+`Mod_Timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 `last_login` datetime NOT NULL,
-primary key (`id`)
+primary key (`Id`)
 );
 
 create table `auth_audit_log` (
-`id` int(1000) NOT NULL AUTO_INCRAMENT,
-`time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-`username` varchar(20) NOT NULL,
-`user_ip` varchar(20) NOT NULL,
-`status` tinyint(1) NOT NULL
-primary key (`id`)
+`Id` int(1000) NOT NULL AUTO_INCRAMENT,
+`Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+`Username` varchar(20) NOT NULL,
+`User_ip` varchar(20) NOT NULL,
+`Status` tinyint(1) NOT NULL
+primary key (`Id`)
 );
 
 create table `logsee_serverstatus` (
-`component` varchar(20) NOT NULL,
-`status` tinyint(1) NOT NULL
-primary key (`component`)
+`Component` varchar(20) NOT NULL,
+`Status` tinyint(1) NOT NULL
+primary key (`Component`)
 );
 
 
