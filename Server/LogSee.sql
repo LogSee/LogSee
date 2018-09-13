04825fcfb64dbd1a3e81f9c4a3440416d1cd0988
Ainsey11 <ainsey11@users.noreply.github.com>
2018-09-13 16:25:50 +0100

    Creating columns for Retention Time and Initial Auth keys

--- Server/LogSee.sql
+++ Server/LogSee.sql
@@ -6,12 +6,10 @@
 
 
 -- Dumping database structure for LogSee
-DROP DATABASE IF EXISTS `LogSee`;
 CREATE DATABASE IF NOT EXISTS `LogSee` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `LogSee`;
 
 -- Dumping structure for table LogSee.Alerts
-DROP TABLE IF EXISTS `Alerts`;
 CREATE TABLE IF NOT EXISTS `Alerts` (
   `AlertID` int(11) NOT NULL AUTO_INCREMENT,
   `ClientID` int(11) NOT NULL,
@@ -29,20 +27,33 @@ CREATE TABLE IF NOT EXISTS `Alerts` (
 
 -- Data exporting was unselected.
 -- Dumping structure for table LogSee.Clients
-DROP TABLE IF EXISTS `Clients`;
 CREATE TABLE IF NOT EXISTS `Clients` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `UserID` int(11) DEFAULT NULL,
-  `Authorized` enum('Y','N') DEFAULT NULL,
   `IP` varchar(50) DEFAULT NULL,
+  `InitalAuth` enum('Failed','Awaiting Approval','Approved','Denied') DEFAULT 'Failed',
+  `Live` enum('Y','N') DEFAULT NULL,
   PRIMARY KEY (`ID`),
   KEY `UserID_FK` (`UserID`),
   CONSTRAINT `UserID_FK` FOREIGN KEY (`UserID`) REFERENCES `Users` (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- Data exporting was unselected.
+-- Dumping structure for table LogSee.InitialAuthKeys
+CREATE TABLE IF NOT EXISTS `InitialAuthKeys` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `UserID` int(11) DEFAULT NULL,
+  `Key` varchar(255) DEFAULT NULL,
+  `DateCreated` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `DateExpires` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `Key` (`Key`),
+  KEY `FK_InitialAuthKeys_Users` (`UserID`),
+  CONSTRAINT `FK_InitialAuthKeys_Users` FOREIGN KEY (`UserID`) REFERENCES `Users` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 -- Dumping structure for table LogSee.LogFiles
-DROP TABLE IF EXISTS `LogFiles`;
 CREATE TABLE IF NOT EXISTS `LogFiles` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `ClientID` int(11) DEFAULT NULL,
@@ -57,9 +68,9 @@ CREATE TABLE IF NOT EXISTS `LogFiles` (
 
 -- Data exporting was unselected.
 -- Dumping structure for table LogSee.LogSeries
-DROP TABLE IF EXISTS `LogSeries`;
 CREATE TABLE IF NOT EXISTS `LogSeries` (
   `ID` int(11) NOT NULL,
+  `RetentionTime` int(11) NOT NULL,
   `LogFileID` int(11) DEFAULT NULL,
   `Data` longtext,
   `DateAdded` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
@@ -70,7 +81,6 @@ CREATE TABLE IF NOT EXISTS `LogSeries` (
 
 -- Data exporting was unselected.
 -- Dumping structure for table LogSee.ServerStatus
-DROP TABLE IF EXISTS `ServerStatus`;
 CREATE TABLE IF NOT EXISTS `ServerStatus` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`ID`)
@@ -78,7 +88,6 @@ CREATE TABLE IF NOT EXISTS `ServerStatus` (
 
 -- Data exporting was unselected.
 -- Dumping structure for table LogSee.Users
-DROP TABLE IF EXISTS `Users`;
 CREATE TABLE IF NOT EXISTS `Users` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Live` enum('Y','N') DEFAULT 'Y',
@@ -87,6 +96,11 @@ CREATE TABLE IF NOT EXISTS `Users` (
   `Hash` varchar(255) DEFAULT NULL,
   `Salt` varchar(255) DEFAULT NULL,
   `DateCreated` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `Forename` varchar(255) DEFAULT NULL,
+  `Surname` varchar(255) DEFAULT NULL,
+  `Email` varchar(255) DEFAULT NULL,
+  `LastLogin` timestamp NULL DEFAULT NULL,
+  `LastIP` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
