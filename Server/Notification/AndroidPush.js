b8db3e6630e4286d4835387f3b100f2e5ce4743c
P0pzi <tburtun@gmail.com>
2018-09-14 00:00:53 +0100

    Helping Ainsey cos he anal

--- Server/Notification/AndroidPush.js
+++ Server/Notification/AndroidPush.js
@@ -1,13 +1,17 @@
 var request = require('request');
+var fs = require('fs');
+var path = require('path');
 
-function SendAndroidPushNotification(MessageTitle,Message) {
-  var config = JSON.parse(fs.readFileSync(path.join(__dirname + '../config.json'), 'utf8'));
-     if (config.Notifications.Simplepush.Enabled){
-       var SimplepushAPIKey = config.Notifications.Simplepush.Key;
-       var url = 'https://api.simplepush.io/send/${SimplePushAPIKey}/${MessageTitle}/${Message}' ;
-       request(url, function (error, response) {
-           console.log('error:', error); // Print the error if one occurred
-	   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-       });
-   }
-}
+module.exports = {
+  SendAndroidPushNotification: function(MessageTitle, Message) {
+    var config = JSON.parse(fs.readFileSync(path.join(__dirname + '/../config.json'), 'utf8'));
+      if (config.Notifications.Simplepush.Enabled) {
+        var SimplepushAPIKey = config.Notifications.Simplepush.Key;
+        var url = `https://api.simplepush.io/send/${SimplePushAPIKey}/${MessageTitle}/${Message}`;
+        request(url, function (error, response) {
+            console.log('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response, response.statusCode); // Print the response status code if a response was received
+        });
+    }
+  }
+};
