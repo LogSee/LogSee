35da473b652da94bb4b88f00e9c9c5cf4e761c7b
ainsey11 <ainsey11@gmail.com>
2018-09-13 14:06:33 -0700

    making random log file generator to help us test things, Ainseys Node virginity has been taken!

---
+++ Client/DevTools/LogCreator.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var filename = Math.random().toString(36).substr(2, 5) + '.log';
+
+fs.open(filename, 'w', function (err, file) {
+  if (err) throw err;
+  console.log('Created file. File name is: '+ filename);
+});
+
+function InsertJunk(){
+var RandomString = Math.random().toString(36).substr(2, 15);
+fs.appendFile(filename, RandomString + '\r\n' , function (err) {
+  if (err) throw err;
+  console.log('Added Log Line! Log name is: ' + filename);
+})
+}
+
+setInterval(InsertJunk, 1500);
