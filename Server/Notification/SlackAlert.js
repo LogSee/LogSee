79611181a7e69c7cb050a8f973728ed550e4f00a
ainsey11 <ainsey11@gmail.com>
2018-09-13 17:57:27 -0700

    trying to make slack buttons work

--- Server/Notification/SlackAlert.js
+++ Server/Notification/SlackAlert.js
@@ -7,8 +7,28 @@ module.exports = {
         var config = JSON.parse(fs.readFileSync(path.join(__dirname + '/../config.json'), 'utf8'));
         if (config.Notifications.Slack.Enabled) {
             var SlackWebhookURL = config.Notifications.Slack.SlackWebhookURL;
+            var LogSeeURL = config.WebUI.IP + ':' + config.WebUI.Port
             var headers = {"Content-type": 'application/json'};
-            var payload = {'text': 'Hello world'};
+            var payload = {
+            "text": Message,
+            "attachments": [
+                {
+                    "text": "View on LogSee",
+                    "fallback": "View on Logsee",
+                    "color": "#3AA3E3",
+                    "attachment_type": "default",
+                    "actions": [
+                        {
+                            "name" : "View",
+                            "text": "Open Logsee Web Panel",
+                            "type": "button",
+                            "value": "Open LogSee Web Panel",
+                            "url": "http://LogSeeURL"
+                        }
+                    ]
+                }
+            ]
+            };
             payload = JSON.stringify(payload);
 
             request.post({url: SlackWebhookURL, body: payload, headers: headers}, function(err, res){
