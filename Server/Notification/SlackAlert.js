9fb100f0dc689750fcea61f762dfd7d2d825b14a
P0pzi <tburtun@gmail.com>
2018-09-14 01:38:25 +0100

    Fixed

--- Server/Notification/SlackAlert.js
+++ Server/Notification/SlackAlert.js
@@ -6,14 +6,14 @@ module.exports = {
     SendSlackNotification: function(Message) {
         var config = JSON.parse(fs.readFileSync(path.join(__dirname + '/../config.json'), 'utf8'));
         if (config.Notifications.Slack.Enabled) {
-            var SlackWebhookURL = config.Notifications.Slack.SlackWebhookURL
-            var payload = {'text':Message}
-            payload = JSON.stringify(payload)
-            var headers = {"Content-type": 'application/json'}
+            var SlackWebhookURL = config.Notifications.Slack.SlackWebhookURL;
+            var headers = {"Content-type": 'application/json'};
+            var payload = {'text': 'Hello world'};
+            payload = JSON.stringify(payload);
 
-            request.post({url: SlackWebhookURL, payload: payload, headers: headers}, function(err, res){
-                if(err){console.log(err)}
-                if(res){console.log(res.body)}
+            request.post({url: SlackWebhookURL, body: payload, headers: headers}, function(err, res){
+                if(err){console.log('Error', err)}
+                if(res){console.log('Resp', res.body, res.statusCode)}
             })
         }
     }
