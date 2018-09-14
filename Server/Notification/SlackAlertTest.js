3a05e703805647dcbcbe747f051a225b6855f390
ainsey11 <ainsey11@gmail.com>
2018-09-13 18:17:11 -0700

    muhahahaha pointless commit

--- Server/Notification/SlackAlertTest.js
+++ Server/Notification/SlackAlertTest.js
@@ -3,4 +3,5 @@ var test = require(path.join(__dirname + '/SlackAlert.js'));
 
 
 console.log(typeof test.SendSlackNotification);
+console.log("now we are equal on commits xD")
 test.SendSlackNotification("I like trains lololol");
