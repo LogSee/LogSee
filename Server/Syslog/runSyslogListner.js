53245b3527dd63358d1343cffff591706cfbd640
Ainsey11 <ainsey11@users.noreply.github.com>
2018-09-16 04:57:15 +0100

    made a start on the syslog input stream

---
+++ Server/Syslog/runSyslogListner.js
@@ -0,0 +1,10 @@
+const SyslogServer = require("syslog-server");
+const server = new SyslogServer();
+ 
+server.on("message", (value) => {
+    //console.log(value.date);     // the date/time the message was received
+    console.log(value.host);     // the IP address of the host that sent the message
+    console.log(value.message);  // the syslog message
+});
+ 
+server.start();
