063abe362dce024dbc748b11b64ea22c00cd9ffa
P0pzi <tburtun@gmail.com>
2018-10-09 21:00:46 +0100

    Angular being a suck.

--- Client/WebUI/src/app/configure.service.ts
+++ Client/WebUI/src/app/configure.service.ts
@@ -4,21 +4,24 @@ import { HttpHeaders } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 
 const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type':  'application/json'
-  })
+	headers: new HttpHeaders({
+		'Content-Type':  'application/json'
+	})
 };
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 
 export class ConfigureService {
-  constructor(
-    private http: HttpClient,
+	constructor(
+		private http: HttpClient,
+	){}
 
-  ) { }
-    PingInterval() {
-      return this.http.post('/api/configure/pinginterval', '{"pinginterval": 200}');
-    };
+	PingInterval() {
+	  return this.http.post('/api/configure/pinginterval', '{"pinginterval": 200}');
+	};
+	GetConfig() {
+	  return this.http.get('/api/getConfig'); // Uhh yea, is weird. I hate angular.
+	};
 };
