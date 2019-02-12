5743bcf10514a975688f9fa22dd3467b1794887e
P0pzi <tburtun@gmail.com>
2019-02-12 23:40:24 +0000

    Mongo-Sample

---
+++ Server/Database/LogSee-Mongo.js
@@ -0,0 +1,14 @@
+var MongoClient = require('mongodb').MongoClient;
+
+
+// Connect to the db
+
+MongoClient.connect("mongodb://home.nerdcave.us:27017/exampleDb", function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    dbo.createCollection("customers", function(err, res) {
+      if (err) throw err;
+      console.log("Collection created!");
+      db.close();
+    });
+  }); 
